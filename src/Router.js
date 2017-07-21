4e7d8f855af59e30b552fb4c625bd547fa6cb677
yonseung kim <yonseung.kim@gmail.com>
2017-07-21 13:43:59 -0400

    add new route

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import ChatBar from './components/ChatBar';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
       <Scene key = "auth">
         <Scene key="login" component={LoginForm} title="Login" />
+      </Scene>
+      
+      <Scene key = "main">
+        <Scene key="chatbar" component={ChatBar} title="Chat" />
       </Scene>
     </Router>
   );
