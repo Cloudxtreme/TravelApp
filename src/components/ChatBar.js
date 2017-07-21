65e1889082c0737dd34ab3a6e78817805e32feb9
yonseung kim <yonseung.kim@gmail.com>
2017-07-21 13:45:34 -0400

    initial commit

---
+++ src/components/ChatBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Actions } from 'react-native-router-flux';
+import { Container, Content, Button, Text, Icon } from 'native-base';
+
+class ChatBar extends Component {
+  render() {
+    return (
+      <Container>
+      </Container>
+    );
+  }
+}
+
+export default ChatBar;
