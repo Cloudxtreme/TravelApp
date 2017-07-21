74ee6bfb1e38bd0e73e69582c003b3096db935fa
yonseung kim <yonseung.kim@gmail.com>
2017-07-21 01:22:51 -0400

    add facebook sdk

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,17 +2,31 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loginUser } from '../actions';
 import { Container, Content, Button, Text, Icon } from 'native-base';
-
+import FBSDK, { LoginButton, AccessToken } from 'react-native-fbsdk';
+  
 class LoginForm extends Component {
 
   render() {
     return (
       <Container>
         <Content style={styles.contentStyle}>
-          <Button iconLeft block style={{marginBottom: 10}}>
-            <Icon active name="logo-facebook" />
-            <Text>Login with Facebook</Text>
-          </Button>
+          <LoginButton
+            onLoginFinished={
+              (error, result) => {
+                if (error) {
+                  alert("login has error: " + result.error);
+                } else if (result.isCancelled) {
+                  alert("login is cancelled.");
+                } else {
+                  AccessToken.getCurrentAccessToken().then(
+                    (data) => {
+                      alert(data.accessToken.toString())
+                    }
+                  )
+                }
+              }
+            }
+            onLogoutFinished={() => alert("logout.")}/>
         </Content>
       </Container>
     );
